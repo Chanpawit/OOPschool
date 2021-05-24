1ee9ad0e1e77f8792703f8473cd4a4481d9df369
Chanpawit <78028508+Chanpawit@users.noreply.github.com>
2021-05-24 15:14:06 +0700

    Add files via upload

---
+++ python_OOP_NewSchool.py
@@ -0,0 +1,15 @@
+# New school.py
+
+#import python_OOP
+from python_OOP_school1 import *
+
+st1 = Student('Albert', 'Johnson')
+print(st1)
+
+st2 = Student('Bill', 'Gates')
+print(st2)
+
+stp1 = SpecialStudent('T','E','P')
+
+teacher1 = Teacher('Cya')
+print(teacher1.fullname)
